a72a90851e725170e2a193b68e451ef672e68be4
eunyoung <eunyoung.park@lemonhc.com>
2023-09-06 16:39:18 +0900

    router 설정, git 초기 css, 레이아웃 작업중

---
+++ client/src/components/common/CommonHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header>
+      <h1><a href="#" class="header-logo">Logo</a></h1>
+      <div class="header-right">
+        <ul>
+          <li><a href="#">LOGIN</a></li>
+          <li><a href="#">JOIN</a></li>
+          <li><a href="#">CART</a></li>
+        </ul>
+        <div>
+          <button>논알콜 제주누보 구매</button>
+          <button>양조장 예약</button>
+        </div>
+      </div>
+    </header>
+    <nav>
+      <ul>
+       <li><a href="#">EVENT</a></li>
+       <li><a href="#">BARREL SERIES</a></li>
+       <li><a href="#">SHOP</a></li>
+       <li><a href="#">BREWERY</a></li>
+      </ul>
+    </nav>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "CommonHeader",
+  components : 'CommonHeader'
+}
+</script>
+<style>
+  
+</style>
